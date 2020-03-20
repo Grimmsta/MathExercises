a196feac72e8031f9fad6b2f44dcaf45736f0894
Grimmsta <grimmsta2k15@gmail.com>
2020-03-20 14:08:53 +0100

    no message

---
+++ MathExercises/Assets/Scripts/ProjectileDistance.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ProjectileDistance : MonoBehaviour
+{
+    public Transform player;
+    public Transform point;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(player.position, point.position);
+    }
+}
